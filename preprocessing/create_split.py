63cf50500a67cac3173cda29688fda85603336e4
bradenhurl <bradenhurl@gmail.com>
2019-03-11 10:22:10 -0400

    Removed requirement for any objects to be in view for creating split. Also removed unnecessary logging.

--- preprocessing/create_split.py
+++ preprocessing/create_split.py
@@ -8,19 +8,10 @@ def create_split(base_dir, alt_perspective_dir, split):
 
     filename = base_dir + '/{}.txt'.format(split)
     with open(filename, 'w+') as f:
-        files = os.listdir(data_dir)
-        num_files = len(files)
-        file_idx = 0
         for file in os.listdir(data_dir):
             filepath = data_dir + '/' + file
             idx = int(os.path.splitext(file)[0])
-            if os.stat(filepath).st_size != 0:
-                printIdx(idx, f)
-
-            sys.stdout.write("\rWorking on idx: {} / {}".format(
-                    file_idx + 1, num_files))
-            sys.stdout.flush()
-            file_idx = file_idx + 1
+            printIdx(idx, f)
                 
 
 def printIdx(idx, f):
