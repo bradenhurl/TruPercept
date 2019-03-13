2534137ec192f6fdd08a516355641a3a84f7bc4d
bradenhurl <bradenhurl@gmail.com>
2019-03-12 22:24:22 -0400

    Fixed trust detection obj being in a list. Never returns None now, deals with it by returning empty list.

--- trust_utils.py
+++ trust_utils.py
@@ -22,29 +22,25 @@ def getEgoTrustObject(base_dir, idx, entity_id):
     ego_dir = base_dir + '/ego_object/'
     ego_detection = obj_utils.read_labels(ego_dir, idx)
     ego_detection[0].score = 1.0
-    ego_tDet = TrustDetection(entity_id, ego_detection, 1.0)
+    ego_tDet = TrustDetection(entity_id, ego_detection[0], 1.0)
     return ego_tDet
 
 
 def createTrustObjects(base_dir, idx, entity_id, detections):
-
-    if detections == None:
-        return None
-
-    certainties = certainty_utils.load_certainties(base_dir, idx)
-
-    c_idx = 0
     trust_detections = []
-    for det in detections:
-        tDet = TrustDetection(entity_id, det, certainties[c_idx])
-        trust_detections.append(tDet)
-        c_idx += 1
 
     # Add ego object (self)
-    ego_dir = base_dir + '/ego_object/'
-    ego_detection = obj_utils.read_labels(ego_dir, idx)
-    ego_detection[0].score = 1.0
     ego_tDet = getEgoTrustObject(base_dir, idx, entity_id)
     trust_detections.append(ego_tDet)
+
+    # Convert detections to trust objects
+    if detections is not None:
+        certainties = certainty_utils.load_certainties(base_dir, idx)
+
+        c_idx = 0
+        for det in detections:
+            tDet = TrustDetection(entity_id, det, certainties[c_idx])
+            trust_detections.append(tDet)
+            c_idx += 1
 
     return trust_detections
