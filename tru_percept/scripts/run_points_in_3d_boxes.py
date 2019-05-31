a55a93e6861e374ff8ef99a980ee8cea0427cdc3
bradenhurl <bradenhurl@gmail.com>
2019-05-31 17:24:06 -0400

    Added script for running points in 3d boxes and tracking the run time.

---
+++ tru_percept/scripts/run_points_in_3d_boxes.py
@@ -0,0 +1,7 @@
+import tru_percept.tru_percept.points_in_3d_boxes as points_3d
+import time
+
+start = time.time()
+points_3d.compute_points_in_3d_boxes()
+end = time.time()
+print("\nTime for points_in_3d_boxes: ", end - start)
