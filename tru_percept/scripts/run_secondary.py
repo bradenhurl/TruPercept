acacefe02f8b065ec0aee132603f580bb98e44d0
bradenhurl <bradenhurl@gmail.com>
2019-06-05 12:51:38 -0400

    Added msg evals to secondary computations.

--- tru_percept/scripts/run_secondary.py
+++ tru_percept/scripts/run_secondary.py
@@ -1,9 +1,11 @@
 import vehicle_trust as v_trust
+import message_evaluations as msg_evals
 import final_detections as f_dets
 import eval_utils
 import config as cfg
 
 # Perform remaining tru_percept operations
+msg_evals.compute_message_evals()
 v_trust.calculate_vehicle_trusts()
 f_dets.compute_final_detections()
 eval_utils.run_kitti_native_script(cfg.SCORE_THRESHOLD)
