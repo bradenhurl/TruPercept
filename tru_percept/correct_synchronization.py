40d5a320831189407a570897326f21f991fdb6c6
bradenhurl <bradenhurl@gmail.com>
2019-07-19 13:37:59 -0400

    Separated functions for obtaining detections from any perspective which can be synchronized to any perspective.

--- tru_percept/correct_synchronization.py
+++ tru_percept/correct_synchronization.py
@@ -64,33 +64,17 @@ def correct_synchro():
 
         # Do for all the alternate perspectives
         for entity_str in persp_dirs:
-            perspect_dir = os.path.join(alt_pers_dir, entity_str)
-            if not os.path.isdir(perspect_dir):
-                continue
+            persp_dir = os.path.join(alt_pers_dir, entity_str)
 
-            det_dir = perspect_dir + '/{}'.format(cfg.PREDICTIONS_SUBDIR)
-            if not os.path.isdir(det_dir):
-                continue
+            persp_det = get_synchronized_dets(persp_dir, cfg.DATASET_DIR, idx, dict_ego_gt)
 
-            gt_dir = perspect_dir + '/label_aug_2'
-            if not os.path.isdir(gt_dir):
+            if persp_det == -1:
                 continue
-
-            det_filepath = perspect_dir + '/{}/{:06d}.txt'.format(cfg.PREDICTIONS_SUBDIR, idx)
-            if not os.path.isfile(det_filepath):
-                continue
-
-            gt_filepath = perspect_dir + '/label_aug_2/{:06d}.txt'.format(idx)
-            if not os.path.isfile(gt_filepath):
-                continue
-
-            persp_det = obj_utils.read_labels(det_dir, idx, results=True, synthetic=False)
-            persp_gt = obj_utils.read_labels(gt_dir, idx, results=False, synthetic=True)
 
             # Make sure directory exists if we've made it this far
-            out_dir = perspect_dir + '/predictions_synchro/'
+            out_dir = persp_dir + '/predictions_synchro/'
             std_utils.make_dir(out_dir)
-
+            
             # If there are no detections then stop but make empty file
             # since empty file exists for predictions
             if persp_det == None:
@@ -98,66 +82,110 @@ def correct_synchro():
                 with open('{}/{:06d}.txt'.format(out_dir, idx), 'w+') as f:
                     continue
 
-            # Should just match 1 to 1 with highest match
-            cfg.IOU_MATCHING_THRESHOLD = 0.5
-
-            # Match predictions to their ground truth object
-            max_ious, iou_indices = matching_utils.get_iou3d_matches(persp_gt, persp_det)
-            max_speed = -1
-            max_speed_idx = -1
-            min_ry_diff = sys.maxsize
-            for obj_idx in range(0, len(iou_indices)):
-                if iou_indices[obj_idx] != -1:
-                    matched_speed = persp_gt[int(iou_indices[obj_idx])].speed
-                    persp_det[obj_idx].speed = matched_speed
-                    persp_det[obj_idx].id = persp_gt[int(iou_indices[obj_idx])].id
-                    if matched_speed > max_speed:
-                        max_speed = matched_speed
-                        max_speed_idx = obj_idx
-
-                        # Should also try to take vehicle which turns the least (as it will affect speed/distance)
-                        ry_diff = abs(persp_gt[int(iou_indices[obj_idx])].ry - persp_det[obj_idx].ry)
-                        if min_ry_diff > ry_diff:
-                            min_ry_diff = ry_diff
-
-            # Adjust detection positions using velocity
-            # if any detection was matched with speed > 0
-            if max_speed > 0:
-                key = persp_det[max_speed_idx].id
-                if key in dict_ego_gt:
-                    # Convert to ego vehicle coordinates
-                    p_utils.to_world(persp_det, perspect_dir, idx)
-                    p_utils.to_perspective(persp_det, cfg.DATASET_DIR, idx)
-                    p_utils.to_world(persp_gt, perspect_dir, idx)
-                    p_utils.to_perspective(persp_gt, cfg.DATASET_DIR, idx)
-
-                    # Get the time from the object with the highest speed
-                    obj1 = dict_ego_gt[key]
-                    obj2 = persp_det[max_speed_idx]
-                    pos_diff = np.asarray(obj2.t) - np.asarray(obj1.t)
-                    pos_diff = pos_diff.reshape((1,3))
-                    time = math.sqrt(np.dot(pos_diff, pos_diff.T)) / obj2.speed
-
-                    # Convert back to perspective coordinates then save
-                    p_utils.to_world(persp_det, cfg.DATASET_DIR, idx)
-                    p_utils.to_perspective(persp_det, perspect_dir, idx)
-
-                    # Adjust all the detections based on the offset time and their own speed
-                    for obj in persp_det:
-                        # First need to convert ry back to proper angle
-                        theta = np.arctan2(np.cos(obj.ry), -np.sin(obj.ry))
-                        # Next need to extract x/y components
-                        unit_vec = np.asarray([np.cos(theta), np.sin(theta)])
-
-                        # Lastly use the calculated time offset to create a distance
-                        # offset and add it to the obj position
-                        dist = time * obj.speed
-                        offset = unit_vec * -dist
-                        obj.t = (obj.t[0] + offset[1], obj.t[1], obj.t[2] + offset[0])
-
             std_utils.save_objs_to_file(persp_det, idx, out_dir, True)
 
 
     print("Finished synchronizing perspectives.")
+
+# Attempts to synchronize detections from one vehicle to the time of
+# another vehicle
+# Returns all detections, synchronized or not
+# If gt file doesn't exist, returns -1
+# if no detections, returns None
+# persp_dir: Base perspective directory where detections are loaded from
+# to_persp_dir: Base perspective directory detections are being synchronized with
+def get_synchronized_dets(persp_dir, to_persp_dir, idx, to_persp_dict_gt=None):
+    if not os.path.isdir(persp_dir):
+        return -1
+
+    det_dir = persp_dir + '/{}'.format(cfg.PREDICTIONS_SUBDIR)
+    if not os.path.isdir(det_dir):
+        return -1
+
+    gt_dir = persp_dir + '/label_aug_2'
+    if not os.path.isdir(gt_dir):
+        return -1
+
+    det_filepath = persp_dir + '/{}/{:06d}.txt'.format(cfg.PREDICTIONS_SUBDIR, idx)
+    if not os.path.isfile(det_filepath):
+        return -1
+
+    if to_persp_dict_gt is None:
+        # Create dictionary for quickly obtaining speed of object
+        to_persp_aug_label_dir = to_persp_dir + '/label_aug_2'
+        to_persp_gt = obj_utils.read_labels(to_persp_aug_label_dir, idx, results=False, synthetic=True)
+        to_persp_dict_gt = {}
+        for obj in to_persp_gt:
+            to_persp_dict_gt[obj.id] = obj
+
+    gt_filepath = persp_dir + '/label_aug_2/{:06d}.txt'.format(idx)
+    if not os.path.isfile(gt_filepath):
+        return -1
+
+    persp_det = obj_utils.read_labels(det_dir, idx, results=True, synthetic=False)
+    persp_gt = obj_utils.read_labels(gt_dir, idx, results=False, synthetic=True)
+
+    if persp_det == None:
+        return persp_det
+
+    # Should just match 1 to 1 with highest match
+    cfg.IOU_MATCHING_THRESHOLD = 0.5
+
+    # Match predictions to their ground truth object
+    max_ious, iou_indices = matching_utils.get_iou3d_matches(persp_gt, persp_det)
+    max_speed = -1
+    max_speed_idx = -1
+    min_ry_diff = sys.maxsize
+    for obj_idx in range(0, len(iou_indices)):
+        if iou_indices[obj_idx] != -1:
+            matched_speed = persp_gt[int(iou_indices[obj_idx])].speed
+            persp_det[obj_idx].speed = matched_speed
+            persp_det[obj_idx].id = persp_gt[int(iou_indices[obj_idx])].id
+            if matched_speed > max_speed:
+                max_speed = matched_speed
+                max_speed_idx = obj_idx
+
+                # Should also try to take vehicle which turns the least (as it will affect speed/distance)
+                ry_diff = abs(persp_gt[int(iou_indices[obj_idx])].ry - persp_det[obj_idx].ry)
+                if min_ry_diff > ry_diff:
+                    min_ry_diff = ry_diff
+
+    # Adjust detection positions using velocity
+    # if any detection was matched with speed > 0
+    if max_speed > 0:
+        key = persp_det[max_speed_idx].id
+        if key in to_persp_dict_gt:
+            # Convert to ego vehicle coordinates
+            p_utils.to_world(persp_det, persp_dir, idx)
+            p_utils.to_perspective(persp_det, cfg.DATASET_DIR, idx)
+            p_utils.to_world(persp_gt, persp_dir, idx)
+            p_utils.to_perspective(persp_gt, cfg.DATASET_DIR, idx)
+
+            # Get the time from the object with the highest speed
+            obj1 = to_persp_dict_gt[key]
+            obj2 = persp_det[max_speed_idx]
+            pos_diff = np.asarray(obj2.t) - np.asarray(obj1.t)
+            pos_diff = pos_diff.reshape((1,3))
+            time = math.sqrt(np.dot(pos_diff, pos_diff.T)) / obj2.speed
+
+            # Convert back to perspective coordinates then save
+            p_utils.to_world(persp_det, cfg.DATASET_DIR, idx)
+            p_utils.to_perspective(persp_det, persp_dir, idx)
+
+            # Adjust all the detections based on the offset time and their own speed
+            for obj in persp_det:
+                # First need to convert ry back to proper angle
+                theta = np.arctan2(np.cos(obj.ry), -np.sin(obj.ry))
+                # Next need to extract x/y components
+                unit_vec = np.asarray([np.cos(theta), np.sin(theta)])
+
+                # Lastly use the calculated time offset to create a distance
+                # offset and add it to the obj position
+                dist = time * obj.speed
+                offset = unit_vec * -dist
+                obj.t = (obj.t[0] + offset[1], obj.t[1], obj.t[2] + offset[0])
+
+    return persp_det
+
 
 correct_synchro()
