7f975ea4478fbe23e3de764fa9691a0e9b0199a2
bradenhurl <bradenhurl@gmail.com>
2019-07-20 22:06:10 -0400

    Rearranged config to group together similar options.

--- tru_percept/config.py
+++ tru_percept/config.py
@@ -8,10 +8,56 @@ import tru_percept.false_detections as false_dets
 SCENE_NUM = 3
 DATASET_DIR = os.path.expanduser('~') + '/GTAData/TruPercept/object_tru_percept{}/training'.format(SCENE_NUM)
 
-# Certainty threshold scores
+
+# ********************************************************************** #
+# Experimentation configuration
+
+# Certainty/visibility threshold scores
 # TODO Probabilistic approach to certainty
 gamma_upper = 100
 gamma_lower = 0
+
+# Filters final detections by this score threshold to speed up kitti evaluation
+SCORE_THRESHOLD = 0.09
+
+# Minimum iou to be considered a match
+IOU_MATCHING_THRESHOLD = 0.1
+
+# False detections to use
+# None for no detections
+# malicious_front for adding detections in front of vehicles
+FALSE_DETECTIONS_TYPE = None#'malicious_front'
+FALSE_DETECTIONS_SUBDIR = 'false_detections'
+FALSE_DETECTIONS = false_dets.load_false_dets(DATASET_DIR, \
+                        FALSE_DETECTIONS_SUBDIR, FALSE_DETECTIONS_TYPE)
+
+# Default trust value for first-time vehicles
+DEFAULT_VEHICLE_TRUST_VAL = 0.5
+
+# Message evaluation value for negative matches
+NEG_EVAL_SCORE = 0.0
+
+# Use regular vs filtered (for distance, etc) ground truth labels
+EVALUATE_UNFILTERED = False
+
+# Aggregation method
+# 0 is averaging
+# 1 is additive
+# 2 is based on msg evaluations
+# 3 is believe all with score 1.0
+# 4 is believe all with score 1.0 unless non-matching, then confidence
+# 5 is believe all ego which are matches
+# 6 is believe all ego which have matches at score = 1, believe other ego
+#           with a score they were detected with
+# 7 is sanity check (passes through ego vehicle objects with same score)
+# 9 Same as 2 but average msg eval with ego vehicle detection confidence
+AGGREGATE_METHOD = 0
+
+# Attempts to synchronize the detections by matching and using velocity
+SYNCHRONIZE_DETS = False
+
+# ********************************************************************** #
+# For testing
 
 # Min/max indices which will be run (for running tests primarily)
 # If set to 0 and sys.maxsize, respectively, all indices will be run
@@ -27,47 +73,18 @@ if TEST_IDX != -1:
     MIN_IDX = TEST_IDX
     MAX_IDX = TEST_IDX
 
-# Only skips indices for inference!!!
-# Indices to skip in case of bugs, problems
-INDICES_TO_SKIP = {}
-if SCENE_NUM == 3:
-    INDICES_TO_SKIP = {267,268,269,270}#Bugged for pedestrian detection
-    #INDICES_TO_SKIP += {12,13,14}#Detections not properly synchronized
-
-SCORE_THRESHOLD = 0.01
-
-IOU_MATCHING_THRESHOLD = 0.1
-
-# False detections to use
-# None for no detections
-# malicious_front for adding detections in front of vehicles
-FALSE_DETECTIONS_TYPE = 'malicious_front'
-FALSE_DETECTIONS_SUBDIR = 'false_detections'
-FALSE_DETECTIONS = false_dets.load_false_dets(DATASET_DIR, \
-                        FALSE_DETECTIONS_SUBDIR, FALSE_DETECTIONS_TYPE)
-
-# Default trust value for first-time vehicles
-DEFAULT_VEHICLE_TRUST_VAL = 0.5
-
-# Message evaluation value for negative matches
-NEG_EVAL_SCORE = 0.0
-
-CUDA_DEVICE = '0'
-
+# Set to false to only load ground truth instead of predictions
 USE_RESULTS = True
 
-# Aggregation method
-# 0 is averaging
-# 1 is additive
-# 2 is based on msg evaluations
-# 3 is believe all with score 1.0
-# 4 is believe all with score 1.0 unless non-matching, then confidence
-# 5 is believe all ego which are matches
-# 6 is believe all ego which have matches at score = 1, believe other ego
-#           with a score they were detected with
-# 7 is sanity check (passes through ego vehicle objects with same score)
-# 9 Same as 2 but average msg eval with ego vehicle detection confidence
-AGGREGATE_METHOD = 0
+# Set and initialize logging
+LOG_LVL = logging.DEBUG
+LOG_FILE = DATASET_DIR + '/log.txt'
+# Initialize logging
+logging.basicConfig(filename=LOG_FILE, level=LOG_LVL, format='%(levelname)s: %(filename)s(%(lineno)d): %(message)s')
+
+
+# ********************************************************************** #
+# Don't need to change these ones
 
 # Subdirectories for storing intermediate steps
 POINTS_IN_3D_BOXES_DIR = 'points_in_3d_boxes'
@@ -86,23 +103,29 @@ if not USE_RESULTS:
 # Area filtered subdir for kitti evaluation
 FINAL_DETS_SUBDIR_AF = 'final_detections_area_filtered'
 
-# Use regular vs filtered (for distance, etc) ground truth labels
-EVALUATE_UNFILTERED = False
-if EVALUATE_UNFILTERED:
-    LABEL_DIR = 'label_2'
-else:
-    LABEL_DIR = 'label_filtered_2'
-
 SYNCHRONIZED_PREDS_DIR = 'predictions_synchro'
-SYNCHRONIZE_DETS = False
 
 KITTI_EVAL_SUBDIR = 'kitti_native_eval'
 
 AVOD_OUTPUT_DIR = 'predictions'
 PREDICTIONS_SUBDIR = 'predictions'
 
-# Set and initialize logging
-LOG_LVL = logging.DEBUG
-LOG_FILE = DATASET_DIR + '/log.txt'
-# Initialize logging
-logging.basicConfig(filename=LOG_FILE, level=LOG_LVL, format='%(levelname)s: %(filename)s(%(lineno)d): %(message)s')
+# Use regular vs filtered (for distance, etc) ground truth labels
+if EVALUATE_UNFILTERED:
+    LABEL_DIR = 'label_2'
+else:
+    LABEL_DIR = 'label_filtered_2'
+
+
+# ********************************************************************** #
+# Set once
+
+# Set this to proper device then leave it
+CUDA_DEVICE = '0'
+
+# Only skips indices for inference!!!
+# Indices to skip in case of bugs, problems
+INDICES_TO_SKIP = {}
+if SCENE_NUM == 3:
+    INDICES_TO_SKIP = {267,268,269,270}#Bugged for pedestrian detection
+    #INDICES_TO_SKIP += {12,13,14}#Detections not properly synchronized
